7484bd7879e93b1d5d5ee5322851cae9599d6855
martin chan <osiutino@gmail.com>
2011-05-09 00:03:02 +0800

    allow inoutput

--- src/com/example/pixbibluetooth/BluetoothService.java
+++ src/com/example/pixbibluetooth/BluetoothService.java
@@ -1,6 +1,8 @@
 package com.example.pixbibluetooth;
 
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 import java.util.UUID;
 
 import android.bluetooth.BluetoothAdapter;
@@ -8,58 +10,168 @@ import android.bluetooth.BluetoothDevice;
 import android.bluetooth.BluetoothServerSocket;
 import android.bluetooth.BluetoothSocket;
 import android.content.Context;
+import android.os.Bundle;
 import android.os.Handler;
+import android.os.Message;
 import android.util.Log;
 
 public class BluetoothService {
     // Debugging
-    private static final String TAG = "BluetoothChatService";
+    private static final String TAG = "PixBiBluetoothClientService";
     private static final boolean D = true;
     private static final UUID MY_UUID = UUID.fromString("00001101-0000-1000-8000-00805F9B34FB");
 
 	private final BluetoothAdapter mAdapter;
 	private final Handler mHandler;
     private ConnectThread mConnectThread;
+    private ConnectedThread mConnectedThread;
+    
+    
+    // Constants that indicate the current connection state
+    public static final int STATE_NONE = 0;       // we're doing nothing
+    public static final int STATE_LISTEN = 1;     // now listening for incoming connections
+    public static final int STATE_CONNECTING = 2; // now initiating an outgoing connection
+    public static final int STATE_CONNECTED = 3;  // now connected to a remote device
+    private int mState;
     
 	public BluetoothService(Context context, Handler handler) {
         mAdapter = BluetoothAdapter.getDefaultAdapter();
+        mState = STATE_NONE;
         mHandler = handler;
     }
+	/**
+     * Set the current state of the chat connection
+     * @param state  An integer defining the current connection state
+     */
+    private synchronized void setState(int state) {
+        if (D) Log.d(TAG, "setState() " + mState + " -> " + state);
+        mState = state;
+        mHandler.obtainMessage(PixBiBluetoothClient.MESSAGE_STATE_CHANGE, state, -1).sendToTarget();
+    }
+    /**
+     * Return the current connection state. */
+    public synchronized int getState() {
+        return mState;
+    }
+
     public synchronized void start() {
-        if (D) Log.d(TAG, " ++ ConnectThread start ++");
+        Log.d(TAG, " ++ ConnectThread start ++");
 
         // Cancel any thread attempting to make a connection
         if (mConnectThread != null) {mConnectThread.cancel(); mConnectThread = null;}
-
+      
+        // Cancel any thread currently running a connection
+        if (mConnectedThread != null) {mConnectedThread.cancel(); mConnectedThread = null;}
+        
+        setState(STATE_LISTEN);
     }
-	public synchronized void stop() {
-	        if (D) Log.d(TAG, " ++ ConnectThread stop ++ ");
 
-	        if (mConnectThread != null) {
-	            mConnectThread.cancel();
-	            mConnectThread = null;
-	        }
-	 }
-	 public synchronized void connected(BluetoothSocket socket, BluetoothDevice	 device) {
-	        if (D) Log.d(TAG, "++ connected to XBee!!! ++");
-
-	        // Cancel the thread that completed the connection
-	        if (mConnectThread != null) {mConnectThread.cancel(); mConnectThread = null;}
-	        
-	 }
     public synchronized void connect(BluetoothDevice device) {
-        if (D) Log.d(TAG, "connect to: " + device);
+        Log.d(TAG, "connect to: " + device);
 
         // Cancel any thread attempting to make a connection
-        //if (mState == STATE_CONNECTING) {
+        if (mState == STATE_CONNECTING) {
             if (mConnectThread != null) {mConnectThread.cancel(); mConnectThread = null;}
-        //}
-
+        }
+        
+        // Cancel any thread currently running a connection
+        if (mConnectedThread != null) {mConnectedThread.cancel(); mConnectedThread = null;}
+        
         // Start the thread to connect with the given device
         mConnectThread = new ConnectThread(device);
         mConnectThread.start();
-        
+  
+        setState(STATE_CONNECTING);        
     }
+    public synchronized void connected(BluetoothSocket socket, BluetoothDevice	 device) {
+    	Log.d(TAG,"Device Name: "+ device.getName());
+        Log.d(TAG, "++ connected to XBee!!! ++");
+
+        // Cancel the thread that completed the connection
+        if (mConnectThread != null) {mConnectThread.cancel(); mConnectThread = null;}
+        
+        // Cancel any thread currently running a connection
+        if (mConnectedThread != null) {mConnectedThread.cancel(); mConnectedThread = null;}
+        
+        // Start the thread to manage the connection and perform transmissions
+        mConnectedThread = new ConnectedThread(socket);
+        mConnectedThread.start();
+        
+        // Send the name of the connected device back to the UI Activity
+        Message msg = mHandler.obtainMessage(PixBiBluetoothClient.MESSAGE_DEVICE_NAME);
+        Bundle bundle = new Bundle();
+        bundle.putString(PixBiBluetoothClient.DEVICE_NAME, device.getName());
+        msg.setData(bundle);
+        mHandler.sendMessage(msg);
+        
+        setState(STATE_CONNECTED);
+    }
+	public synchronized void stop() {
+        Log.d(TAG, " ++ ConnectThread stop ++ ");
+
+        if (mConnectThread != null) {
+            mConnectThread.cancel();
+            mConnectThread = null;
+        }
+        
+        if (mConnectedThread != null) {
+            mConnectedThread.cancel();
+            mConnectedThread = null;
+        }
+        
+        setState(STATE_NONE);
+	}
+	
+	/**
+     * Write to the ConnectedThread in an unsynchronized manner
+     * @param out The bytes to write
+     * @see ConnectedThread#write(byte[])
+     */
+    public void write(byte[] out) {
+        // Create temporary object
+        ConnectedThread r;
+        // Synchronize a copy of the ConnectedThread
+        synchronized (this) {
+            if (mState != STATE_CONNECTED) return;
+            r = mConnectedThread;
+        }
+        // Perform the write unsynchronized
+        r.write(out);
+    }
+	
+    /**
+     * Indicate that the connection attempt failed and notify the UI Activity.
+     */
+    private void connectionFailed() {
+    	Log.d(TAG, " ++ connection Failed ++ ");
+    	
+        // Send a failure message back to the Activity
+        Message msg = mHandler.obtainMessage(PixBiBluetoothClient.MESSAGE_TOAST);
+        Bundle bundle = new Bundle();
+        bundle.putString(PixBiBluetoothClient.TOAST, "Unable to connect device");
+        msg.setData(bundle);
+        mHandler.sendMessage(msg);
+        
+        // Start the service over to restart listening mode
+        BluetoothService.this.start();
+    }
+    /**
+     * Indicate that the connection was lost and notify the UI Activity.
+     */
+    private void connectionLost() {
+    	Log.d(TAG, " ++ connection Lost ++ ");
+    	
+        // Send a failure message back to the Activity
+        Message msg = mHandler.obtainMessage(PixBiBluetoothClient.MESSAGE_TOAST);
+        Bundle bundle = new Bundle();
+        bundle.putString(PixBiBluetoothClient.TOAST, "Device connection was lost");
+        msg.setData(bundle);
+        mHandler.sendMessage(msg);
+        
+        // Start the service over to restart listening mode
+    	BluetoothService.this.start();
+    }
+
     private class ConnectThread extends Thread {
         private final BluetoothSocket mmSocket;
         private final BluetoothDevice mmDevice;
@@ -96,7 +208,7 @@ public class BluetoothService {
                 } catch (IOException e2) {
                     Log.e(TAG, "ERROR: unable to close() socket during connection failure", e2);
                 }
-                //connectionFailed();
+                connectionFailed();
       
                 Log.e(TAG, "ERROR: Unable to connect device");
                 return;
@@ -116,6 +228,87 @@ public class BluetoothService {
                 mmSocket.close();
             } catch (IOException e) {
                 Log.e(TAG, "ERROR: close() of connect socket failed", e);
+            }
+        }
+    }
+    /**
+     * This thread runs during a connection with a remote device.
+     * It handles all incoming and outgoing transmissions.
+     */
+    private class ConnectedThread extends Thread {
+        private final BluetoothSocket mmSocket;
+        private final InputStream mmInStream;
+        private final OutputStream mmOutStream;
+
+        public ConnectedThread(BluetoothSocket socket) {
+            Log.d(TAG, "create ConnectedThread");
+            mmSocket = socket;
+            InputStream tmpIn = null;
+            OutputStream tmpOut = null;
+
+            // Get the BluetoothSocket input and output streams
+            try {
+                tmpIn = socket.getInputStream();
+                tmpOut = socket.getOutputStream();
+            } catch (IOException e) {
+                Log.e(TAG, "temp sockets not created", e);
+            }
+
+            mmInStream = tmpIn;
+            mmOutStream = tmpOut;
+        }
+
+        public void run() {
+            Log.i(TAG, "BEGIN mConnectedThread");
+            byte[] buffer = new byte[1024];
+            int bytes;
+
+            // Keep listening to the InputStream while connected
+            while (true) {
+                try {
+                    // Read from the InputStream
+                    bytes = mmInStream.read(buffer);
+
+                    // Send the obtained bytes to the UI Activity
+                    mHandler.obtainMessage(PixBiBluetoothClient.MESSAGE_READ, bytes, -1, buffer).sendToTarget();
+
+                    String readMessage = new String(buffer,0,bytes);
+                    Log.i(TAG, "read message: "+ readMessage );
+                    
+                } catch (IOException e) {
+                    Log.e(TAG, "disconnected", e);
+                    connectionLost();
+                    // Start the service over to restart listening mode
+                    BluetoothService.this.start();
+                    break;
+                }
+            }
+        }
+
+        /**
+         * Write to the connected OutStream.
+         * @param buffer  The bytes to write
+         */
+        public void write(byte[] buffer) {
+            try {
+                mmOutStream.write(buffer);
+
+                // Share the sent message back to the UI Activity
+                mHandler.obtainMessage(PixBiBluetoothClient.MESSAGE_WRITE, -1, -1, buffer).sendToTarget();
+                
+                String writeMessage = new String(buffer);
+                Log.i(TAG, "write message: "+ writeMessage );
+                
+            } catch (IOException e) {
+                Log.e(TAG, "Exception during write", e);
+            }
+        }
+
+        public void cancel() {
+            try {
+                mmSocket.close();
+            } catch (IOException e) {
+                Log.e(TAG, "close() of connect socket failed", e);
             }
         }
     }
--- src/com/example/pixbibluetooth/PixBiBluetoothClient.java
+++ src/com/example/pixbibluetooth/PixBiBluetoothClient.java
@@ -11,9 +11,15 @@ import android.bluetooth.BluetoothAdapter;
 import android.bluetooth.BluetoothDevice;
 import android.bluetooth.BluetoothSocket;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
- 
+import android.view.View.OnClickListener; 
+
 public class PixBiBluetoothClient extends Activity {
      
      private static final String TAG = "PixBiBluetoothClient";
@@ -21,7 +27,24 @@ public class PixBiBluetoothClient extends Activity {
      private BluetoothAdapter mBluetoothAdapter = null;
      private BluetoothSocket btSocket = null;
      private OutputStream outStream = null;
+     private Button mReConnBtn = null;
+     private Button mDisConnBtn = null;
      
+     
+     // Message types sent from the BluetoothService Handler
+     public static final int MESSAGE_STATE_CHANGE = 1;
+     public static final int MESSAGE_READ = 2;
+     public static final int MESSAGE_WRITE = 3;
+     public static final int MESSAGE_DEVICE_NAME = 4;
+     public static final int MESSAGE_TOAST = 5;
+     
+     // Key names received from the BluetoothChatService Handler
+     public static final String DEVICE_NAME = "device_name";
+     public static final String TOAST = "toast";
+     
+     // Name of the connected device
+     private String mConnectedDeviceName = null;
+     private TextView rMsgBox = null;
      private BluetoothService btService = null;
      
      // Well known SPP UUID (will *probably* map to
@@ -37,7 +60,9 @@ public class PixBiBluetoothClient extends Activity {
      public void onCreate(Bundle savedInstanceState) {
           super.onCreate(savedInstanceState);
           setContentView(R.layout.main);
- 
+          
+          rMsgBox = (TextView)findViewById(R.id.rMsgBox);
+          rMsgBox.setText("Hello world!\n");
           if (D)
                Log.e(TAG, "+++ ON CREATE +++");
  
@@ -47,7 +72,6 @@ public class PixBiBluetoothClient extends Activity {
                finish();
                return;
           }
-
      }
  
      @Override
@@ -70,7 +94,27 @@ public class PixBiBluetoothClient extends Activity {
  
      private void setupClient() {
     	 Log.d(TAG, "setupClient()");
-    	 btService = new BluetoothService(this,null);
+    	 
+    	 mReConnBtn = (Button) findViewById(R.id.reconnBtn);
+    	 mReConnBtn.setOnClickListener(new OnClickListener(){
+    		public void onClick(View v){
+    	          if (btService != null) btService.stop();
+            	  btService.start();
+            	  connectDevice();
+    	          Log.e(TAG, "--- CLICKED RECONNECT BUTTON ---");
+    		}
+    	 });
+    	 
+    	 mDisConnBtn = (Button) findViewById(R.id.disconnBtn);
+    	 mDisConnBtn.setOnClickListener(new OnClickListener(){
+    		public void onClick(View v){
+    	          if (btService != null) btService.stop();
+    	          Log.e(TAG, "--- CLICKED DISCONNECT BUTTON ---");
+    		}
+    	 });
+    	 
+    	 btService = new BluetoothService(this,mHandler);
+    	 
 	}
      private void connectDevice() {
          // Get the BluetoothDevice object
@@ -91,57 +135,6 @@ public class PixBiBluetoothClient extends Activity {
         	  btService.start();
         	  connectDevice();
           }
-          // When this returns, it will 'know' about the server,
-          // via it's MAC address.
-        
-          
-          /*
-          try {
-               btSocket = device.createRfcommSocketToServiceRecord(MY_UUID);
-          } catch (IOException e) {
-               Log.e(TAG, "ON RESUME: Socket creation failed.", e);
-          }
-          Log.e(TAG,"btSocket: " + btSocket);
-
-          mBluetoothAdapter.cancelDiscovery();
-
-          // Blocking connect, for a simple client nothing else can
-          // happen until a successful connection is made, so we
-          // don't care if it blocks.
-          boolean connectStatus = false;
-          try {
-               btSocket.connect();
-               connectStatus = true;
-               Log.e(TAG, "ON RESUME: BT connection established, data transfer link open.");               
-          } catch (IOException e) {
-               try {
-                    btSocket.close();
-               } catch (IOException e2) {
-                    Log.e(TAG,"ON RESUME: Unable to close socket during connection failure", e2);                    
-               }
-          }
-          if(!connectStatus) {
-        	  Log.e(TAG,"ON RESUME: Error return .");
-        	  return;
-          }
-        	          	  
-          // Create a data stream so we can talk to server.
-          if (D)
-               Log.e(TAG, "+ ABOUT TO SAY SOMETHING TO SERVER +");
- 
-          try {
-               outStream = btSocket.getOutputStream();
-          } catch (IOException e) {
-               Log.e(TAG, "ON RESUME: Output stream creation failed.", e);
-          }
- 
-          String message = "Hello message from client to server.";
-          byte[] msgBuffer = message.getBytes();
-          try {
-               outStream.write(msgBuffer);
-          } catch (IOException e) {
-               Log.e(TAG, "ON RESUME: Exception during write.", e);
-          }*/
      }
  
      @Override
@@ -150,22 +143,56 @@ public class PixBiBluetoothClient extends Activity {
  
           if (D)
                Log.e(TAG, "- ON PAUSE -");
- /*
-          if (outStream != null) {
-               try {
-                    outStream.flush();
-               } catch (IOException e) {
-                    Log.e(TAG, "ON PAUSE: Couldn't flush output stream.", e);
-               }
-          }
- 
-          try  {
-               btSocket.close();
-          } catch (IOException e2) {
-               Log.e(TAG, "ON PAUSE: Unable to close socket.", e2);
-          }*/
      }
- 
+
+     // The Handler that gets information back from the BluetoothChatService
+     private final Handler mHandler = new Handler() {
+         @Override
+         public void handleMessage(Message msg) {
+             switch (msg.what) {
+             case MESSAGE_STATE_CHANGE:
+                 if(D) Log.i(TAG, "MESSAGE_STATE_CHANGE: " + msg.arg1);
+                 switch (msg.arg1) {
+                 case BluetoothService.STATE_CONNECTED:
+                     //setStatus(getString(R.string.title_connected_to, mConnectedDeviceName));
+                     //mConversationArrayAdapter.clear();
+                	 rMsgBox.append("Bluetooth connected.\n");
+                     break;
+                 case BluetoothService.STATE_CONNECTING:
+                     //setStatus(R.string.title_connecting);
+                	 rMsgBox.append("Bluetooth connecting..\n");
+                     break;
+                 case BluetoothService.STATE_NONE:
+                     //setStatus(R.string.title_not_connected);
+                	 rMsgBox.append("Bluetooth not connected.\n");
+                     break;
+                 }
+                 break;
+             case MESSAGE_WRITE:
+                 //byte[] writeBuf = (byte[]) msg.obj;
+                 // construct a string from the buffer
+                 //String writeMessage = new String(writeBuf);
+                 //mConversationArrayAdapter.add("Me:  " + writeMessage);
+                 break;
+             case MESSAGE_READ:
+                 byte[] readBuf = (byte[]) msg.obj;
+                 // construct a string from the valid bytes in the buffer
+                 String readMessage = new String(readBuf, 0, msg.arg1);
+                 rMsgBox.append("Read Message: " + readMessage+"\n");
+                 break;
+             case MESSAGE_DEVICE_NAME:
+                 // save the connected device's name
+                 mConnectedDeviceName = msg.getData().getString(DEVICE_NAME);
+                 Toast.makeText(getApplicationContext(), "Connected to "
+                                + mConnectedDeviceName, Toast.LENGTH_SHORT).show();
+                 break;
+             case MESSAGE_TOAST:
+                 Toast.makeText(getApplicationContext(), msg.getData().getString(TOAST),
+                                Toast.LENGTH_SHORT).show();
+                 break;
+             }
+         }
+     };
      @Override
      public void onStop() {
           super.onStop();
